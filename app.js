7039a755b0731cc6f4ddbd0602605d987dc85dfb
Khamz1 <khamzzaty@mail.ru>
2025-03-06 11:46:53 +0300

    Я сделаль хасамБэк

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let cors = require('cors');
+let path = require('path');
+let fs = require('fs');
+const mongoose = require("mongoose");
+
+let app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://algo-crm:AlgoSalSabil2024@algo-crm.m4ao4.mongodb.net/',{}).then(()=>
+console.log("Connected to the database!"))
+    .catch((err)=>console.log("DB Err",err));
+app.listen(8080,()=>{
+    console.log("Server started on port 8080");
+});
