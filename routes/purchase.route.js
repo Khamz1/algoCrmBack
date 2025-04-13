e2c1168e96d6055e12c4b70b1017ce5d8eeb5163
Yanarkaev <hasssambek@gmail.com>
2025-04-13 18:09:07 +0300

    коммит на всякий

--- routes/purchase.route.js
+++ routes/purchase.route.js
@@ -4,7 +4,7 @@ const { purchaseController } = require("../controllers/purchase.controller");
 
 route.get('/purchase', purchaseController.getPurchase);
 route.get('/purchase/:id', purchaseController.getPurchaseById);
-route.delete('/products/:id', purchaseController.deletePurchase)
+route.delete('/purchase/:id', purchaseController.deletePurchase)
 route.post('/purchase', purchaseController.addPurchase)
 
 module.exports = route
